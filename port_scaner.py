2e119cc66000a99a6fec143565db17a06d2556dd
Guanapi <planeta.chompi18@gmail.com>
2025-04-20 15:45:45 +0200

    args added

--- port_scaner.py
+++ port_scaner.py
@@ -2,19 +2,44 @@ import socket
 import argparse
 
 
-def scan_ports(host, port):
+def scan_ports(host, ports):
+    print(f"Scanning ports on {ports}...")
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.settimeout(10)
-            result = sock.connect_ex((host, port))
+        for port in ports:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.settimeout(1)
+                port = int(port)
+                result = sock.connect_ex((host, port))
 
-            if result == 0:
-                print(f"Port {port} is open")
-            else:
-                print(f"Port {port} is closed")
+                if result == 0:
+                    print(f"Port {port} is open")
+                else:
+                    print(f"Port {port} is closed")
     except Exception as e:
         print(f"Error scanning port {port}: {e}")
 
+def clean_ports(string_ports):
+    cleaned_ports = []
+    
+    for port in string_ports.split(","):
+        port = port.strip()
+        if "-" in port:
+            start, end = port.split("-")
+            cleaned_ports.extend(range(int(start), int(end) + 1))
+        else:
+            cleaned_ports.append(int(port))
+    print(f"Cleaned ports: {cleaned_ports}")
+    return sorted(cleaned_ports)
+# Arguments
 
-scan_ports("164.234.23.1", 80)
+parser = argparse.ArgumentParser(description="Port Scanner")
+parser.add_argument("-t", "--target", required=True, help="Target IP address")
+parser.add_argument("-p", "--ports", default= "1-1024", help="Comma-separated list of ports to scan")
+args = parser.parse_args()
+
+
+ports = clean_ports(args.ports)
+scan_ports(args.target, ports)
+# Example usage:
+
 
